63a1be435feb01ebd22fc83a0c745c35d531bdb7
lukaszozimek <contact@lukaszozimek.com>
2020-03-17 19:51:12 +0100

    Improve database configuration

--- pkg/model/model.go
+++ pkg/model/model.go
@@ -24,8 +24,8 @@ func init() {
 	dbName := util.GetEnvOrDefault("DB_NAME", "'db_organization_service'")
 
 	dbHost := util.GetEnvOrDefault("DB_HOST", "'127.0.0.1'")
-
-	dbUri := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s", dbHost, username, dbName, password) //Build connection string
+	dbPort := util.GetEnvOrDefault("DB_PORT", "'5455'")
+	dbUri := fmt.Sprintf("host=%s port=%s user=%s db	name=%s sslmode=disable password=%s", dbHost, dbPort, username, dbName, password) //Build connection string
 	fmt.Println(dbUri)
 
 	conn, err := gorm.Open("postgres", dbUri)
